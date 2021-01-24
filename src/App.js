a5235738b72d54b5c60391e62215ab69923fd5da
Paula Bonini <paula_almeidabonini@hotmail.com>
2021-01-24 00:22:30 -0300

    media queries ajustments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './components/Nav.css'
 import FormValidator from './components/FormValidator';
 import './components/FormValidator.css';
 
+
 function App() {
   return (
     <div className="container">
--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -2,13 +2,32 @@ import React from 'react';
 import Cards from 'react-credit-cards';
 import "react-credit-cards/es/styles-compiled.css";
 
+const initialState = {
+  cvc: '***',
+  expiry: '00/00',
+  focus: '',
+  name: 'NOME DO TITULAR',
+  number: '**** **** **** ****',
+  numberErr: "",
+  nameErr: "",
+  expiryErr: "",
+  cvcErr: "",
+  quantityErr: ""
+};
+
 export default class PaymentForm extends React.Component {
+
   state = {
     cvc: '***',
     expiry: '00/00',
     focus: '',
     name: 'NOME DO TITULAR',
     number: '**** **** **** ****',
+    numberErr: "",
+    nameErr: "",
+    expiryErr: "",
+    cvcErr: "",
+    quantityErr: ""
   };
 
   handleInputFocus = (e) => {
@@ -20,7 +39,53 @@ export default class PaymentForm extends React.Component {
     
     this.setState({ [name]: value });
   }
-  
+
+  validate = () => {
+    let numberErr = "";
+    let nameErr = "";
+    let expiryErr = "";
+    let cvcErr = "";
+    let quantityErr = "";
+
+    if (!this.state.number.includes(" ")) {
+        numberErr = "Número de cartão inválido";
+    }
+
+    if (!this.state.name.includes(" ")) {
+        nameErr = "Insira seu nome completo";
+    }
+
+    if (!this.state.expiry.includes("/")) {
+        expiryErr = "Data inválida";
+    }
+
+    if (this.state.cvc.length < 3) {
+        cvcErr = "Código inválido";
+    }
+
+    if (!this.state.quantity) {
+        quantityErr = "Insira o número de parcelas";
+    }
+
+    if (numberErr || nameErr || expiryErr || cvcErr || quantityErr) {
+        this.setState({ numberErr, nameErr, expiryErr, cvcErr, quantityErr });
+        return false;
+    }
+
+    return true;
+};
+
+handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+        console.log(this.state);
+        //clear form
+        this.setState(initialState);
+    }
+    
+};
+
   render() {
     return (
       <div id="PaymentForm">
@@ -31,68 +96,71 @@ export default class PaymentForm extends React.Component {
           name={this.state.name}
           number={this.state.number}
         />
-        
-        <form action="">
+
+        <form action="" onSubmit={this.handleSubmit}>
+          <div className="label-float">
+            <input
+              type="text"
+              name="number"
+              placeholder=" "
+              onChange={this.handleInputChange}
+              onFocus={this.handleInputFocus}
+              required
+            />
+            <label>Número do cartão</label>
+            <div className="err">{this.state.numberErr}</div>
+          </div>
+          <div className="label-float">
+            <input
+                type="text"
+                name="name"
+                placeholder=" "
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                required
+            />
+            <label> Nome (igual ao cartão)</label>
+            <div className="err">{this.state.nameErr}</div>
+          </div>
+          <div className="flex">
             <div className="label-float">
-                <input
-                    type="number"
-                    name="number"
-                    placeholder=" "
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
-                    required
-                />
-                    <label>Número do cartão</label>
-                </div>
-                <div className="label-float">
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder=" "
-                        onChange={this.handleInputChange}
-                        onFocus={this.handleInputFocus}
-                        required
-                    />
-                    <label> Nome (igual ao cartão)</label>
-                </div>
-                <div className="flex">
-                    <div className="label-float">
-                        <input
-                            type="date"
-                            name="expiry"
-                            placeholder=" "
-                            onChange={this.handleInputChange}
-                            onFocus={this.handleInputFocus}
-                            required
-                        />
-                        <label>Validade</label>
-                    </div>
-                    <div className="label-float">
-                        <input
-                            type="number"
-                            name="cvc"
-                            placeholder=" "
-                            onChange={this.handleInputChange}
-                            onFocus={this.handleInputFocus}
-                            required
-                        />
-                        <label>CVV<i class="fas fa-info-circle"></i></label>
-                    </div>
-                </div>
-                <div>
-                    <select value={this.state.quantity} onChange={this.handleChange}>
-                        <option className="optionDefault" value="none">Número de parcelas</option>
-                        <option value="1">6x R$500,00 sem juros</option>
-                        <option value="2">12x R$1.000,00 sem juros</option>
-                    </select>
-                    <div className="err select">{this.state.quantityErr}</div>
-                    </div>
-                    
-                    <button name="button" type="submit" >CONTINUE</button>
-                
-			</form>
-
-
+              <input
+                type="text"
+                name="expiry"
+                placeholder=" "
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                required
+              />
+              <label>Validade</label>
+              <div className="err">{this.state.expiryErr}</div>
+            </div>
+            <div className="label-float test">
+              <input
+                type="text"
+                name="cvc"
+                placeholder=" "
+                maxLength="3"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+                required
+              />
+              <label>CVV <i class="fas fa-info-circle"></i></label>
+              <div className="err">{this.state.cvcErr}</div>
+            </div>
+          </div>
+          <div>
+            <select value={this.state.quantity} onChange={this.handleChange}>
+              <option className="optionDefault" value="none">Número de parcelas</option>
+              <option value="1">6x R$500,00 sem juros</option>
+              <option value="2">12x R$1.000,00 sem juros</option>
+            </select>
+            <div className="err select">{this.state.quantityErr}</div>
+          </div>
+              
+          <button name="button" type="submit" >CONTINUE</button>
+              
+			  </form>
       </div>
     );
   }
